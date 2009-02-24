76a77f96a18462a486b3c9185f0e8368c1a6f441
Justin Murray <justin@justin-laptop.(none)>
2009-02-24 16:47:48 -0500

    Fixed all of Dean's bugs so the project will build

--- FIRlib/FIRlib.c
+++ FIRlib/FIRlib.c
@@ -3,10 +3,12 @@
 //FIRlib.c
 //Contains the FIR Filter Functions
 
+#include <stdlib.h>
+#include <string.h>
+
 #include "FIRlib.h"
 
-double nextSample(FilterData_t *filter, double nextVal)
-{
+double nextSample(FilterData_t * f, double nextVal) {
 	/*
 	 *	Takes a Filter Struct and the newest sample value and
 	 *	returns the filtered data and updates the sample array which is 
@@ -19,47 +21,42 @@ double nextSample(FilterData_t *filter, double nextVal)
 	 f->samples[f->next_index] = nextVal;
 	 
 	 //this calculates the sum
-	 j = next_index;
-	 for(i = 0;i < filter->num_samples; i++)
-	 {
-	 	sum += coefficients[i]*samples[j]
+	 j = f->next_index;
+	 for(i = 0;i < f->num_samples; i++) {
+	 	sum += f->coefficients[i] * f->samples[j];
 	 	j++;
-	 	if(j == filter->num_samples)
-	 	{
+	 	if(j == f->num_samples) {
 	 		j = 0;
 	 	}
 	 }
 	 
 	 //increments the next index location and resets it to zero if
 	 //it equals SAMPLES
-	 next_index++;
-	 if(next_index = filter->num_samples)
-	 {
-	 	next_index = 0;
-	 }
+	 f->next_index++;
+	 f->next_index %= f->num_samples;
 }
 
-FilterData_t * initializeFilter(int type)
-{
+FilterData_t * initializeFilter(int type) {
 	/*	This function initializes a FilterData struct to be used in the 
 	*	nextSample function.  The coefficients for the filter struct are *specified in the FIRlib.h header config file as well as the number of samples for each filter
 	*/
 	
 	int i;
+	double ir_coeffs[] = IR_COEFFS;
+	double sonar_coeffs[] = SONAR_COEFFS;
 	FilterData_t *f = malloc(sizeof(FilterData_t));
 	
-	for(i = 0; i < MAX_SAMPLES; i++)
-	{
-		    f->samples[i] = 0;
+	for(i = 0; i < MAX_SAMPLES; i++) {
+		f->samples[i] = 0;
 	}
 	
-	if(type == IR){
-		f->coefficients = IR_COEFFS;
-		num_samples = IR_SAMPLES;
-	}
-	else if(type == SONAR){
-		f->coefficients = SONAR_COEFFS;
-		num_samples = SONAR_SAMPLES;
+	if(type == IR) {
+		memcpy(f->coefficients, ir_coeffs, IR_SAMPLES * sizeof(double));
+		f->num_samples = IR_SAMPLES;
+	} else if(type == SONAR) {
+		memcpy(f->coefficients, sonar_coeffs, SONAR_SAMPLES * sizeof(double));
+		f->num_samples = SONAR_SAMPLES;
 	}
 	f->next_index = 0;
+	return f;
 }
