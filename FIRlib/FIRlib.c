05d39ce5177d44c48e7e62b1a67b01ec28810f2b
Dean Pantages <dpantages@Dean-mac.botlab.cs.pitt.edu>
2009-02-24 13:44:34 -0500

    modifed the initialize function and fixed the next sample function due to the changes made to the initialize function.  Additionally added #defines for the type of filter that we want and changes the struct so the number of samples can vary

--- FIRlib/FIRlib.c
+++ FIRlib/FIRlib.c
@@ -3,6 +3,7 @@
 //FIRlib.c
 //Contains the FIR Filter Functions
 
+#include "FIRlib.h"
 
 double nextSample(FilterData_t *filter, double nextVal)
 {
@@ -19,11 +20,11 @@ double nextSample(FilterData_t *filter, double nextVal)
 	 
 	 //this calculates the sum
 	 j = next_index;
-	 for(i = 0;i < SAMPLES; i++)
+	 for(i = 0;i < filter->num_samples; i++)
 	 {
 	 	sum += coefficients[i]*samples[j]
 	 	j++;
-	 	if(j == SAMPLES)
+	 	if(j == filter->num_samples)
 	 	{
 	 		j = 0;
 	 	}
@@ -32,7 +33,7 @@ double nextSample(FilterData_t *filter, double nextVal)
 	 //increments the next index location and resets it to zero if
 	 //it equals SAMPLES
 	 next_index++;
-	 if(next_index = SAMPLES)
+	 if(next_index = filter->num_samples)
 	 {
 	 	next_index = 0;
 	 }
@@ -41,19 +42,27 @@ double nextSample(FilterData_t *filter, double nextVal)
 	 
 }
 
-FilterData_t * initializeFilter()
+FilterData_t * initializeFilter(int type)
 {
 	/*	This function initializes a FilterData struct to be used in the 
-	*	nextSample function.  The coefficients for the filter struct are *specified in the FIRlib.h header config file
+	*	nextSample function.  The coefficients for the filter struct are *specified in the FIRlib.h header config file as well as the number of samples for each filter
 	*/
 	
 	int i;
 	FilterData_t *f = malloc(sizeof(FilterData_t));
 	
-	for(i = 0; i < SAMPLES; i++)
+	for(i = 0; i < MAX_SAMPLES; i++)
 	{
 		    f->samples[i] = 0;
 	}
-	f->coefficients = COEFFS;
+	
+	if(type == IR){
+		f->coefficients = IR_COEFFS;
+		num_samples = IR_SAMPLES;
+	}
+	else if(type == SONAR){
+		f->coefficients = SONAR_COEFFS;
+		num_samples = SONAR_SAMPLES;
+	}
 	f->next_index = 0;
 }
--- FIRlib/FIRlib.h
+++ FIRlib/FIRlib.h
@@ -3,22 +3,32 @@
 //FIR Library Header File
 
 //number of filter samples
-#define SAMPLES  16 
+#define MAX_SAMPLES 8
+
+//defines filter type
+#define IR = 1
+#define SONAR = 2
+
+//defines number of samples
+#define IR_SAMPLES 4
+#define SONAR_SAMPLES 8
 
 //Filter Coefficients
-#define COEFFS {-0.0484295452148003,0.0316417725690449,0.0663052386394815,0.0161092502141281,-0.0761706272785959,-0.0417806265179821,0.183762552952241,0.417189375297071,0.417189375297071,0.183762552952241,-0.0417806265179821,-0.0761706272785959,0.0161092502141281,0.0663052386394815,0.0316417725690449,-0.0484295452148003}
+#define S_COEFFS {-0.0484295452148003,0.0316417725690449,0.0663052386394815,0.0161092502141281,-0.0761706272785959,-0.0417806265179821,0.183762552952241,0.417189375297071,0.417189375297071,0.183762552952241,-0.0417806265179821,-0.0761706272785959,0.0161092502141281,0.0663052386394815,0.0316417725690449,-0.0484295452148003}
+#define IR_COEFFS {,,,}
 
 //Filter data struct
 typedef struct _FilterData
 {
-  double coefficients[SAMPLES];
+  int num_samples;
+  double coefficients[MAX_SAMPLES];
   int  next_index;
-  double samples[SAMPLES];
+  double samples[MAX_SAMPLES];
 } FilterData_t;
 
 
 //Next Sample Function see FIRlib.c for implementation details
-double nextSample(Filter *filter, double nextVal);
+double nextSample(FilterData_t *filter, double nextVal);
 
 //Initializes a FilterData_t struct see FIRlib.c for implementation details
-FilterData_t * initializeFilter();
+FilterData_t * initializeFilter(int type);
